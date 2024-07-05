b589d1c50ead78e9061148d8c4a1ee3bf8f48165
Rahul Mamoria <rmamoria@griddynamics.com>
2024-07-05 12:08:14 +0530

    portfolio in process

--- src/app/components/about/skill-data.ts
+++ src/app/components/about/skill-data.ts
@@ -1,12 +1,13 @@
 const skillsData = [
     { id: 1, title: 'React.js', icon: 'R', color: 'bg-blue-500', description: 'JavaScript library for building UIs with reusable components.' },
-    { id: 2, title: 'Next.js', icon: 'N', color: 'bg-gray-200', description: 'React framework for production-ready apps with SSR and static site generation.' },
     { id: 3, title: 'JavaScript', icon: 'JS', color: 'bg-yellow-400', description: 'Versatile language for web dev, known for dynamic capabilities.' },
     { id: 4, title: 'TypeScript', icon: 'TS', color: 'bg-blue-600', description: 'Superset of JS with static typing for maintainable code.' },
     { id: 5, title: 'C++', icon: 'C++', color: 'bg-blue-800', description: 'Powerful language for system/software development and games.' },
     { id: 6, title: 'HTML/CSS', icon: 'H/C', color: 'bg-orange-500', description: 'Fundamental languages for web page structure and styling.' },
     { id: 7, title: 'Node.js', icon: 'N', color: 'bg-green-600', description: "JavaScript runtime for scalable network applications." },
-    { id: 8, title: 'Express.js', icon: 'Ex', color: 'bg-gray-800', description: 'Minimal Node.js framework for robust web and mobile apps.' }
+    { id: 8, title: 'Express.js', icon: 'Ex', color: 'bg-gray-800', description: 'Minimal Node.js framework for robust web and mobile apps.' },
+    { id: 2, title: 'Next.js', icon: 'N', color: 'bg-gray-200', description: 'React framework for production-ready apps with SSR.' },
+
   ];
   
   export default skillsData;
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { ThemeService } from 'src/app/theme.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+
+  darkMode=false;
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.darkMode$.subscribe(darkMode => {
+      this.darkMode = darkMode;
+    });
+  }
 
 }
